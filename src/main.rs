6f4ae151d31887c0b2f9da73aad41e1b93d946ac
Alex6323 <Al3k5@protonmail.com>
2019-01-07 13:32:34 +0100

    Implemented TPS feature.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use std::cmp::min;
 use std::collections::VecDeque;
 use std::env;
+use std::io::{self, Write};
 use std::process;
-use std::thread;
-use std::time::{Duration, Instant};
+use std::time::{Duration, SystemTime};
 
 const ADDRESS: &str = "tcp://192.168.1.5"; //change this to localhost
 const PORT: u16 = 5560;
 const CHANNEL_TX: &'static str = "tx";
+const MOVING_AVG_INTERVAL_MS: u64 = 60000;
 
 struct Arguments {
     program: String,
@@ -43,15 +45,42 @@ fn main() {
             .connect(&address)
             .expect(&format!("Could not connect to publisher '{}'.", address));
 
-        println!("Connected to address '{}'.", address);
+        println!("Connected to publisher: '{}'.", address);
 
         let subscription = CHANNEL_TX.to_string().into_bytes();
         subscriber.set_subscribe(&subscription).unwrap();
 
-        println!("Subscribed to channel: '{:?}'", CHANNEL_TX);
+        println!("Subscribed to channel: '{}'", CHANNEL_TX);
+
+        let mut now: Duration;
+        let interval = Duration::from_millis(MOVING_AVG_INTERVAL_MS);
+        let mut start: Duration;
+        let mut events: VecDeque<Duration> = VecDeque::new();
+        let mut uptime_ms: u64;
+
+        let init = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
 
         loop {
             subscriber.recv_msg(0).unwrap();
+            now = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap();
+
+            start = now - interval;
+            events.push_back(now);
+
+            while events.front().unwrap() < &start {
+                events.pop_front();
+            }
+
+            uptime_ms = (now - init).as_secs() * 1000 + (now - init).subsec_millis() as u64;
+            print!(
+                "\rTPS: {:.2}",
+                events.len() as f64 / (min(MOVING_AVG_INTERVAL_MS, uptime_ms) as f64 / 1000_f64)
+            );
+            io::stdout().flush().unwrap();
         }
     } else {
         process::exit(0);
